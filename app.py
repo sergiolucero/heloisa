25ed4c1e5fe8ea5ebde4489b06bdd2229252a5c5
Sergio Lucero <sergiolucero@gmail.com>
2023-09-10 10:05:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ HumanMessagePromptTemplate.from_template("{question}"),]
 prompt = ChatPromptTemplate.from_messages(messages)
 chain_type_kwargs = {"prompt": prompt}
 #####################################################
+print('KIPÁ MASHUKAO?')
 def main():
     st.title('  Chateando Con Página Web')
     st.subheader('Escribe una dirección Web, haz preguntas y recibe respuestas del sitio')
